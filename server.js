5bb3608d2649d91bf746e4f97414e03745ba2d70
Deepak Singh <deepaksingh567845@gmail.com>
2025-09-23 23:32:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.get('/', (req, res) => {
 });
 
 // start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
